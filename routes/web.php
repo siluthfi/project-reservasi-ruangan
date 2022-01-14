0727c249ba1cf20b4915d2bd464a77d1e08c0d4b
siluthfi <luthfirpl.skenda@gmail.com>
2022-01-14 10:17:02 +0800

    deleted unnecessary file

--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home', [
-        "title" => "Home"
-    ]);
-});
-
-Route::get('/about', function () {
-    return view('about', [
-        "title" => "About"
-    ]);
-});
-
-Route::get('/blog', function () {
-    return view('posts', [
-        "title" => "Posts"
-    ]);
+    return view('welcome', []);
 });
